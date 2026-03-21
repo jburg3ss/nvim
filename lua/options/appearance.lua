72dc9363778ab7393fc4639303e3286526c08a31
user <user@beelzebub>
2026-03-21 19:19:34 +0000

    auto: update appearance.lua

--- lua/options/appearance.lua
+++ lua/options/appearance.lua
@@ -1,16 +1,17 @@
 local cmd = vim.cmd
 
-vim.cmd.colorscheme "moonfly"
+cmd.colorscheme "doric-valley"
+vim.o.background = "dark"
 
 vim.o.winborder = "rounded"
 vim.opt.guicursor = "a:block"
 
-vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#00FF00" }) -- main cursor color
-vim.api.nvim_set_hl(0, "iCursor", { fg = "#000000", bg = "#FF0000" }) -- insert mode color
+-- vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#00FF00" }) -- main cursor color
+-- vim.api.nvim_set_hl(0, "iCursor", { fg = "#000000", bg = "#FF0000" }) -- insert mode color
 
-cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
+-- cmd [[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+-- ]]
